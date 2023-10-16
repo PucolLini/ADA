35da51d443b6c938aa78e896f67980ae1b8f9074
Linia <woloszyn.kari@gmail.com>
2023-10-16 19:50:15 +0200

    Update simulation.adb

--- simulation.adb
+++ simulation.adb
@@ -2,13 +2,17 @@
 -- The students should rename the tasks of producers, consumers, and the buffer
 -- Then, they should change them so that they would fit their assignments
 -- They should also complete the code with constructions that lack there
+
+--Patrycja Nerc 193075
+--Karina Woloszyn 193592
+
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO;
 with Ada.Numerics.Discrete_Random;
 
 procedure Simulation is
-   Number_Of_Products   : constant Integer := 11;
-   Number_Of_Assemblies : constant Integer := 5;
+   Number_Of_Products   : constant Integer := 5;
+   Number_Of_Assemblies : constant Integer := 4;
    Number_Of_Consumers  : constant Integer := 4;
 
    Can_Accept_Assembly  : Boolean := True;
@@ -17,18 +21,21 @@ procedure Simulation is
    subtype Assembly_Type is Integer range 1 .. Number_Of_Assemblies;
    subtype Consumer_Type is Integer range 1 .. Number_Of_Consumers;
 
-      subtype Production_Time_Range is Integer range 3 .. 11;
+      subtype Production_Time_Range is Integer range 3 .. 5;
       subtype Production_Time_Range2 is Integer range 1 .. 4;
       subtype Production_Range is Integer range 1 .. 2 ;
 
-   Product_Name : constant array (Product_Type) of String (1 .. 20) :=
-     ("-----Czujniki-------", "--------Kola--------", "-------Szyby--------",
-      "------Hamulce-------", "-----Kierownica-----", "----Klimatyzacja----",
-      "-Kratki wentylacyjne", "-------Lampy--------", "-------Lustrka------",
-      "-Poduszki powietrza-",  "----Wycieraczki-----");
-   Assembly_Name : constant array (Assembly_Type) of String (1 .. 20) :=
-     ("--BezpieczeÅstwo---", "----Sterowanie------", "-----NapÄÂd-------",
-      "----Chlodzenie-----", "Elementy zewnetrzne");
+   Product_Name : constant array (Product_Type) of String (1 .. 14) :=
+     ("Sensors       ", 
+      "Wheels        ", 
+      "Breaks        ", 
+      "Steering Wheel",
+      "Lamps         ");
+   Assembly_Name : constant array (Assembly_Type) of String (1 .. 17) :=
+     ("Safety           ", 
+      "Steering         ", 
+      "Drive            ", 
+      "Exterior elements");
 
    package Random_Assembly is new Ada.Numerics.Discrete_Random (Assembly_Type);
    type My_Str is new String (1 .. 256);
@@ -66,8 +73,9 @@ procedure Simulation is
       Product_Type_Number_1to3 : Integer;
       Product_Number      : Integer;
       Production          : Integer;
-      Random_Number		  : Integer;
-
+      Random_Number		   : Integer;
+      Product_Before      : Product_Type := Product_Type(Number_Of_Products);
+      
       procedure Dostawa is
       begin
          Random_Production.Reset (G);
@@ -75,26 +83,32 @@ procedure Simulation is
          Random_Range.Reset (G3);
          Random_Number := Random_Range.Random (G3);
          Put_Line("Dostawa started");
+         
 
          if Random_Number = 1 then
-            Product_Type_Number_1to3 := Random_Production_1to3.Random (G2);
-             Put_Line
-              ("Produced product " & Product_Name (Product_Type_Number_1to3) &
-              " number " & Integer'Image (Product_Number));
-            	B.Take (Product_Type_Number_1to3, Product_Number);
+            loop
+               Product_Type_Number_1to3 := Random_Production_1to3.Random (G2);
+               exit when Product_Type_Number_1to3 /= Product_Before;
+            end loop;
+            Put_Line ("Produced product " & Product_Name (Product_Type_Number_1to3));
+            Product_Before := Product_Type_Number_1to3;
          else
-            Product_Type_Number := Random_Production.Random(G);
+            loop
+               Product_Type_Number := Random_Production.Random(G);
+               exit when Product_Type_Number /= Product_Before;
+            end loop;
+            
+            Product_Before := Product_Type_Number;
             Put_Line
-              ("Produced product " & Product_Name (Product_Type_Number) &
-                 " number " & Integer'Image (Product_Number));
+              ("Produced product " & Product_Name (Product_Type_Number));
             B.Take (Product_Type_Number, Product_Number);
          end if;
 
          Product_Number := Product_Number + 1;
+         
       end Dostawa;
 
    begin
-      Put_Line("Start begin");
       accept Start (Product : in Product_Type; Production_Time : in Integer) do
          Random_Production.Reset (G);    --  start random number generator
          Random_Production_1to3.Reset (G2);
@@ -108,12 +122,11 @@ procedure Simulation is
 
       loop
          delay Duration(Random_Production.Random(G));
-         Put_Line("dostawa start");
-         Put_Line("Produced product " & Product_Name (Product_Type_Number) &
-                 " number " & Integer'Image (Product_Number));
+   
+         Put_Line("Produced product " & Product_Name (Product_Type_Number));
          B.Take (Product_Type_Number, Product_Number);
          Product_Number := Product_Number + 1;
-         --Dostawa;
+         Dostawa;
      end loop;
    end Producer;
 
@@ -138,7 +151,6 @@ procedure Simulation is
         (1 .. Number_Of_Consumers) of String (1 .. 4) :=
         ("Audi", "Opel", "Jeep", "Seat");
    begin
-      Put_Line("Costumer started begin");
       accept Start(Consumer_Number : in Consumer_Type; Consumption_Time : in Integer)
       do
          Random_Consumption.Reset (G);   --  ustaw generator
@@ -148,42 +160,40 @@ procedure Simulation is
       Put_Line ("Started consumer " & Consumer_Name (Consumer_Nb));
       Assembly_Number := 1;
       loop
-         Put_Line("Loop in costumer started");
          delay Duration (Random_Consumption.Random (G));
          Random_Assembly.Reset(G2);
          Random_Consumer.Reset(G3);
          Assembly_Type := Random_Assembly.Random (G2);
          Consumer_Nb := Random_Consumer.Random(G3);
          B.Deliver (Assembly_Type, Assembly_Number);
-         --select
+
          if Can_Accept_Assembly then
-            --select
-               Put_Line(Consumer_Name (Consumer_Nb) &
+            Put_Line(Consumer_Name (Consumer_Nb) &
                           ": taken assembly " &
-                          Assembly_Name (Assembly_Type));
-            --or delay 5.0;
-            --end select;
+                       Assembly_Name (Assembly_Type));
+            --show storage
          else
-            Put_Line("Klient wyszedl");
+            Put_Line(Consumer_Name (Consumer_Nb) & 
+                       " left - there was no " & 
+                       Assembly_Name (Assembly_Type));
          end if;
       end loop;
    end Consumer;
 
    task body Buffer is
-      Storage_Capacity : constant Integer := 150;--zwiekszy
+      Storage_Capacity : constant Integer := 50;--zwiekszy
       type Storage_type is array (Product_Type) of Integer;
-      Storage          : Storage_type := (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+      Storage          : Storage_type := (0, 0, 0, 0, 0);
       Assembly_Content : array (Assembly_Type, Product_Type) of Integer :=
-      --  cz,ko,sz,ha,ki,kl,kr,la,lu,po,wy
-        ((1, 0, 0, 2, 0, 0, 0, 1, 1, 2, 2), --bezpieczenstwo
-         (1, 4, 0, 1, 1, 0, 0, 0, 0, 0, 0), -- sterowanie
-         (1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0), -- naped
-         (1, 0, 2, 0, 0, 1, 3, 0, 0, 0, 0), -- chlodzenie
-         (2, 2, 2, 0, 0, 0, 0, 4, 2, 0, 3) -- elem. zewnet.
-        ); -- przsunÄÄ szyby
+     --  cz,ko,ha,ki,la
+        ((1, 0, 2, 0, 1), --bezpieczenstwo
+         (0, 2, 1, 1, 0), -- sterowanie
+         (1, 2, 0, 0, 0), -- naped
+         (2, 0, 0, 0, 2) -- elem. zewnet.
+        );
       Max_Assembly_Content : array (Product_Type) of Integer
-		:= (15, 20, 15, 10, 5, 5, 8, 12, 10, 8, 10);
-      Assembly_Number      : array (Assembly_Type) of Integer := (1, 1, 1, 1, 1);
+		:= (12, 12, 9, 5, 9);
+      Assembly_Number      : array (Assembly_Type) of Integer := (1, 1, 1, 1);
       In_Storage           : Integer                          := 0;
 
       procedure Setup_Variables is
@@ -191,11 +201,11 @@ procedure Simulation is
          for W in Product_Type loop
            -- Max_Assembly_Content (W) := 0;
             for Z in Assembly_Type loop
-             --  if Assembly_Content (Z, W) > Max_Assembly_Content (W) then
-               --   Max_Assembly_Content (W) := Assembly_Content (Z, W);
+              if Assembly_Content (Z, W) > Max_Assembly_Content (W) then
+                  Max_Assembly_Content (W) := Assembly_Content (Z, W);
                   Put_Line("Setup: max asse " & Integer'Image( Max_Assembly_Content (W)));
                   Put_Line("Assembly_Content " & Integer'Image( Assembly_Content (Z, W)));
-              -- end if;
+              end if;
             end loop;
          end loop;
       end Setup_Variables;
@@ -208,11 +218,9 @@ procedure Simulation is
          Lacking_room : Integer;
          MP           : Boolean;                   --  can accept
       begin
-         Put_Line("Can_accept started" & Product_Name(Product));
          if In_Storage >= Storage_Capacity then
             return False;
          end if;
-         -- There is free room in the storage
          Free := Storage_Capacity - In_Storage;
          MP   := True;
          for W in Product_Type loop
@@ -221,26 +229,31 @@ procedure Simulation is
             end if;
          end loop;
          if MP then
-            return True; --  storage has products for arbitrary
-            --  assembly
+            return True;
+         end if;
+         if Storage (Product) = Max_Assembly_Content (Product) then
+               Put_Line("NO SPACE IN STORAGE FOR " & Product_Name(Product)); 
+               Put_Line("STORAGE: " & Integer'Image(Storage(Product)) & " MAX: " & Integer'Image(Max_Assembly_Content(Product)));
+               MP := False;
+               return False;
          end if;
          if Integer'Max(0, Max_Assembly_Content (Product) - Storage (Product)) > 0
          then
-            -- exactly this product lacks
-			 -- czy iloÃÂÃÂÃÂÃÂ ktÃÂÃÂ³rÃÂÃÂ dostarczymy wystarczy aby wydaÃÂÃÂ zamÃÂÃÂ³weinie do klienta?
             return True;
          end if;
-         Lacking_room := 1;                     --  insert current product
+         
+         Lacking_room := 1;  --  insert current product
+         
          for W in Product_Type loop
-            Lacking (W) :=
-              Integer'Max (0, Max_Assembly_Content (W) - Storage (W));
+            Lacking (W) := Integer'Max (0, Max_Assembly_Content (W) - Storage (W));
             Lacking_room := Lacking_room + Lacking (W);
          end loop;
          if Free >= Lacking_room then
             -- there is enough room in storage for arbitrary assembly
             return True;
          else
-            -- no room for this product
+            Put_Line("NO SPACE IN STORAGE FOR " & Product_Name(Product)); 
+            Put_Line("STORAGE: " & Integer'Image(Storage(Product)) & " MAX: " & Integer'Image(Max_Assembly_Content(Product))); 
             return False;
          end if;
       end Can_Accept;
@@ -248,31 +261,28 @@ procedure Simulation is
       function Can_Deliver (Assembly : Assembly_Type) return Boolean is
       begin
          for W in Product_Type loop
-            Put_Line("Schowek check: " & Integer'Image(Storage(W)));
-            Put_Line("Produkty potrzebne check: " & Integer'Image(Assembly_Content (Assembly, W)));
             if Storage (W) < Assembly_Content (Assembly, W) then
-               Put_Line("nie moze dostaczyc");
                Can_Accept_Assembly := False;
                return False;
             end if;
          end loop;
-         Put_Line("moze dostarczyc");
          Can_Accept_Assembly := True;
          return True;
       end Can_Deliver;
 
       procedure Storage_Contents is
       begin
+         Put_Line("+++++++++++++++++++++++++");
          Put_Line("Storage contents: ");
          for W in Product_Type loop
             Put_Line ( Product_Name (W)& " x" & Integer'Image (Storage (W)));
          end loop;
+         Put_Line("+++++++++++++++++++++++++");
       end Storage_Contents;
 
       procedure IsInBufer (Assembly :  in Assembly_Type; Number : out Integer) is
       begin
-          Put_Line("Delivered assembly " & Assembly_Name (Assembly) &
-                       " number " & Integer'Image (Assembly_Number (Assembly)));
+          Put_Line("Delivered assembly " & Assembly_Name (Assembly));
           for W in Product_Type loop
                      Storage (W) := Storage (W) - Assembly_Content (Assembly, W);
                      In_Storage  := In_Storage - Assembly_Content (Assembly, W);
@@ -280,68 +290,62 @@ procedure Simulation is
 
           Number                     := Assembly_Number (Assembly);
           Assembly_Number (Assembly) := Assembly_Number (Assembly) + 1;
-          Storage_Contents;
+          --Storage_Contents;
       end IsInBufer;
-
+      
    begin
       Put_Line ("Buffer started");
       Setup_Variables;
       loop
          select
             accept Take (Product : in Product_Type; Number : in Integer) do
-               Put_Line("Take started" & Product_Name (Product));
+               Put_Line("Take started " & Product_Name (Product));
                if Can_Accept (Product) then
                   Put_Line
-                    ("Accepted product " & Product_Name (Product) & " number " &
-                       Integer'Image (Number)); --konwersja & liczby number na stringa
+                    ("Accepted product " & Product_Name (Product));
                   Storage (Product) := Storage (Product) + 1;
                   In_Storage        := In_Storage + 1;
-                  Storage_Contents;
+                  ---Storage_Contents;
                else
                   Put_Line
-                    ("Rejected product " & Product_Name (Product) & " number " &
-                       Integer'Image (Number));
+                    ("Rejected product " & Product_Name (Product));
                end if;
             end Take;
             or
              accept Deliver (Assembly : in Assembly_Type; Number : out Integer) do
                Put_Line("Deliver started " & Assembly_Name (Assembly));
                if Can_Deliver (Assembly) then
-                  Put_Line("czy moze dostarczyc");
                   IsInBufer(Assembly, Number);
                   Can_Accept_Assembly := True;
                else
+                  Put_Line("There aren't enough products - please wait");
                   for I in 1 .. 4 loop
-                     Put_Line("Petla probujaca " & Integer'Image(I));
+                     Put_Line("Try no. " & Integer'Image(I));
                      if Can_Deliver (Assembly) then
-                        Put_Line("Proba " & Integer'Image(I));
                         IsInBufer(Assembly, Number);
                         exit;
                      else
+                         Put_Line("Please wait");
                         delay 3.0;
                      end if;
                   end loop;
                   Put_Line("Lacking products for assembly " & Assembly_Name (Assembly));
                   Number := 0;
                end if;
-                  --do tablicy brakÃ³w dopisywane sÄ produkty i klient czeka na dostaw
+                  --do tablicy braków dopisywane są produkty i klient czeka na dostaw
             end Deliver;
          end select;
+         Storage_Contents;
       end loop;
+      
    end Buffer;
 
 begin
    for I in 1 .. Number_Of_Products loop
-      Put_Line("Loop1 in last main started");
-      P (I).Start (I, 2);  -- iteracja po tablicy produktÃÂÃÂ³w
-      --delay 1.0;
+      P (I).Start (I, 8);
    end loop;
 
-   Put_Line("Loop1 ended");
-   delay 10.0;
-
    for J in 1 .. Number_Of_Consumers loop
-       Put_Line("Loop2 in last main started");
-      K (J).Start (J, 12); -- iteracja po tablicy konsumentÃÂÃÂ³w
+      K (J).Start (J, 12);
    end loop;
 end Simulation;
